2ef6f964ed5ec93176b5158f315813d5b2ec9976
kimHyeonJu <kimhyesom1202@gmail.com>
2025-08-22 14:21:54 +0900

    [Day6] SSR 실습
    - index 페이지 데이터 패칭 구현
    1. recoMovies : 3개의 추천 영화 데이터를 불러온다.
    2. allMovies : 전체 영화 데이터를 불러온다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,37 @@
-import { ReactNode, useEffect } from 'react';
+// > CSS Modules
+import styles from '@/pages/index.module.css';
+import { ReactNode } from 'react';
 import SearchableLayout from '@/components/searchable-layout';
 import MovieItem from '@/components/movie-item';
-import styles from '@/pages/index.module.css';
-import movies from '@/mock/dummy.json';
 import { InferGetServerSidePropsType } from 'next';
+import fetchMovies from '@/lib/fetch-movies';
+import fetchRandomMovies from '@/lib/fetch-random-movies';
 
-export const getServerSideProps = () => {
-  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
-  
-  console.log('SSR props 입니다.');
-  // 함수는 서버측에서만 실행이 되기 때문에 브라우저에는 출력이 안된다.
-  // 요청이 들어올 때 마다 VSCode의 터미널에 출력된다.
+export const getServerSideProps = async () => {
+  const allMovies = await fetchMovies();
+  const recoMovies = await fetchRandomMovies();
 
-  /*
-    window.location;
-    : ReferenceError: window is not defined 오류발생 → 아래의 Home 컴포넌트와도 같음
-    
-    why?
-    1. window는 즉 브라우저이를 의미
-    2. 서버 환경에서만 실행되는 getServerSideProps 함수 안에서는 브라우저를 읽을 수 없음
-    
-    따라서, window가 undefined가 되어버리고 undefined.location이 되는 것 → window.confirm, window.alert 등 똑같음
-
-    해결) 브라우저 측에서만 실행되는 방법
-    1. useEffect 사용
-    useEffect(() => {
-      console.log(window);
-    }, []);
-  */
-  
-  const data = 'hello';
-  
   return {
     props: {
-      data,
+      allMovies,
+      recoMovies,
     },
   }
 }
 
-export default function Home(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  // InferGetServerSidePropsType : getServerSideProps 함수의 반환값 타입을 자동으로 추론해주는 기능의 타입
-
-  useEffect(() => {
-    console.log(window);
-  }, []);
-
+export default function Home({ allMovies, recoMovies } : InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={styles.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
         <div className={styles.recommend_movie}>
-          { movies.slice(0, 3).map((movie) => <MovieItem key={movie.id} {...movie} />) }
+          {recoMovies.map((movie) => <MovieItem key={movie.id} {...movie} />)}
         </div>
       </section>
       <section>
         <h3>등록된 모든 영화</h3>
         <div className={styles.register_movie}>
-          {movies.map((movie) => <MovieItem key={movie.id} {...movie} />)}
+          {allMovies.map((movie) => <MovieItem key={movie.id} {...movie} />)}
         </div>
       </section>
     </div>
@@ -66,9 +41,3 @@ export default function Home(props: InferGetServerSidePropsType<typeof getServer
 Home.getLayout = (page: ReactNode) => {
   return <SearchableLayout>{ page }</SearchableLayout>
 }
-
-/* 
-  * SSR(서버 사이드 렌더링, Server Side Rendering)
-  : 가장 기본적인 사전 렌더링 방식
-  : 요청이 들어올 때 마다 사전 렌더링을 진행
-*/
