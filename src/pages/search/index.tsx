b7c6c20693e154acf92810be8967174838546ab7
kimHyeonJu <kimhyesom1202@gmail.com>
2025-08-26 12:24:10 +0900

    [Day7] SSG 소개 및 실습
    - 정적 경로에 적용하기

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -7,22 +7,26 @@ import MovieItem from '@/components/movie-item';
 import fetchMovies from '@/lib/fetch-movies';
 import { MovieData } from '@/types';
 
+
 export default function Page() {
   const [filteredMovies, setFilteredMovies] = useState<MovieData[]>([]);
   const router = useRouter();
   const q = router.query.q as string; 
 
   const fetchSearchMovies = async () => {
-    const movies = await fetchMovies(q);
+    const movies = await fetchMovies(q as string);
     setFilteredMovies(movies);
   }
 
   useEffect(() => {
     if (q) {
+      // 검색 결과를 불러오는 로직
       fetchSearchMovies();
     }
-  }, []);
-  
+  }, [q]);
+  /* 
+    * getServerSideProps와 getStaticProps가 없기 때문에 기본적으로 SSG방식으로 동작하는 페이지
+  */
 
   return (
     <div className={styles.container}>
